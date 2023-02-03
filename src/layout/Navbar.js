bfe7d0f6e9c284f04b2ca5dc076c7612b1e70e62
krupk <MaxKR1994>
2023-02-04 00:30:48 +0200

    connected back and front using axios

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
             Full Stack Application
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
@@ -17,10 +17,10 @@ export default function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
 
-          <button class="btn btn-outline-light">Add user</button>
+          <button className="btn btn-outline-light">Add user</button>
         </div>
       </nav>
     </div>
