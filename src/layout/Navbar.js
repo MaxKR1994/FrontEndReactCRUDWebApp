486ea1cf834bea655f4655b8888f541f3fa0e092
krupk <MaxKR1994>
2023-02-03 23:32:52 +0200

    Added navbar from bootstrap

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,33 +1,15 @@
 import React from 'react'
 
-import { Link } from "react-router-dom";
-
 export default function Navbar() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            Full Stack Application
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <Link className="btn btn-outline-light" to="/adduser">
-            Add User
-          </Link>
-        </div>
-      </nav>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Full Stack Application</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
   );
 }
 
