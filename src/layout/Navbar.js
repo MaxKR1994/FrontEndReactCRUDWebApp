2ab731227cb19cacf944135a0bab70e9a2ba19aa
krupk <MaxKR1994>
2023-02-03 23:38:54 +0200

    Added a new button

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -8,6 +8,8 @@ export default function Navbar() {
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
+    <button class="btn btn-outline-light">Add user</button>
   </div>
 </nav>
   );
