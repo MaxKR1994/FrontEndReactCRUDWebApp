6ac862df9b07286696c95341e440d8d8785e2872
krupk <MaxKR1994>
2023-02-04 02:12:52 +0200

    created register form using React

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React from "react";
 
 export default function AddUser() {
   return (
     <div className="container">
-    <div className="row">
-      <div className="col-md-6 offset-md-3 border rouned p-4 mt-2 shadow">
-        <h2 className="text-center m-4">Register User</h2>
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rouned p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Register User</h2>
+          <div className="mb-3">
+            <input
+              type={"text"}
+              className="form=control"
+              placeholder="Enter your name"
+              name="name"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type={"text"}
+              className="form=control"
+              placeholder="Enter your username"
+              name="username"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type={"text"}
+              className="form=control"
+              placeholder="Enter your email"
+              name="email"
+            />
+          </div>
+          <button type="submit" className="btn btn-outline-primary">
+            Submit
+          </button>
+          <button type="submit" className="btn btn-outline-primary">
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
-
